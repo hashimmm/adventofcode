71210471d5b4b2dc4e9a7cdcc874c8a132fa99ea
hashim <hashim.muqtadir@gmail.com>
2017-08-25 02:05:32 +0500

    Comment.

--- day20.rkt
+++ day20.rkt
@@ -3,9 +3,10 @@
 (require data/integer-set)
 
 ;; Racket data structure packages ftw ^_^
-;; Btw the alternative smart way to do this is to sort the lower and upper ranges separately
+;; Btw one nice way to do this is to sort the lower and upper ranges separately
 ;; and zip the sorted lists to make new pairs and you can iterate over these for the same
 ;; results.
+;; Fastest way is to probably just do everything in one iteration.
 
 (define blacklist
   (for/fold ([is (make-range)])
