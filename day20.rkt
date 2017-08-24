22f83188563180b2f118c45d15b17fc55c4277e4
hashim <hashim.muqtadir@gmail.com>
2017-08-24 21:10:28 +0500

    Added day 20

---
+++ day20.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(require data/integer-set)
+
+;; Racket data structure packages ftw ^_^
+;; Btw the alternative smart way to do this is to sort the lower and upper ranges separately
+;; and zip the sorted lists to make new pairs and you can iterate over these for the same
+;; results.
+
+(define blacklist
+  (for/fold ([is (make-range)])
+            ([line (in-lines (open-input-file "day20-input"))])
+    (define numbers (filter-map string->number (regexp-split "-" line)))
+    (if (null? numbers)
+        is
+        (union is (apply make-range numbers)))))
+
+(define whitelist
+  (subtract (make-range 0 4294967295)
+                        blacklist))
+
+;; Part 1
+
+(stream-first whitelist)
+
+;; Part 2
+
+(count whitelist)
